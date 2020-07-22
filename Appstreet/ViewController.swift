a52b257a7dafb19460b160a9a46407e2b32d1123
Jayant Patil <jayant.ptl@gmail.com>
2020-07-22 15:19:10 +0530

    Initial Commit

---
+++ Appstreet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Appstreet
+//
+//  Created by Jayant on 22/07/20.
+//  Copyright © 2020 Jayant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
