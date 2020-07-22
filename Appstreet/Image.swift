30009bdafa8e729b478107076b0ddf84405df8ea
Jayant Patil <jayant.ptl@gmail.com>
2020-07-22 18:51:45 +0530

    Add response processing objects for images

---
+++ Appstreet/Image.swift
@@ -0,0 +1,19 @@
+//
+//  Image.swift
+//  Appstreet
+//
+//  Created by Jayant on 22/07/20.
+//  Copyright © 2020 Jayant. All rights reserved.
+//
+
+import Foundation
+
+class Image: Codable {
+    // Actual response object contains many other parameters. Here processing only which are necessary.
+    let id: String
+    let previewURL: URL
+    let webformatURL: URL
+    let largeImageURL: URL
+    let fullHDURL: URL
+    let imageURL: URL
+}
