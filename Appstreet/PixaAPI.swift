c23d6c39abecd26d566ddf964a4f30abfdf105a9
Jayant Patil <jayant.ptl@gmail.com>
2020-07-22 18:51:45 +0530

    Add response processing objects for images

--- Appstreet/PixaAPI.swift
+++ Appstreet/PixaAPI.swift
@@ -9,19 +9,27 @@
 import Foundation
 
 enum Endpoint: String {
-    case searchImages = ""//q=abcd+xyz&image_type=photo"
+    case searchImages = ""
+}
+
+struct PixaResponse: Codable {
+    let images: [Image]
+
+    enum CodingKeys: String, CodingKey {
+        case images = "hits"
+    }
 }
 
 struct PixaAPI {
     private static let baseURLString = "https://pixabay.com/api/"
     private static let key = "17577339-d40adde472bf1bd9e8a4ad81e"
 
+
     static var searchImagesURL: URL {
         return pixaURL(endpoint: .searchImages, parameters: ["key": key, "q": "computer", "image_type": "photo"])
     }
 
     private static func pixaURL(endpoint: Endpoint, parameters: [String: String]?) -> URL {
-//        return URL(string: "")
         var components = URLComponents(string: baseURLString)!
         var queryItems = [URLQueryItem]()
         if let otherParams = parameters {
